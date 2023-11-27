de9d40b4df8228c757156bf44bedd069079d250e
Lucas Eitel Cereceda <leitel1@uc.cl>
2023-11-26 21:38:09 -0300

    bottom 0 for footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='flex items-center text-3xl w-full justify-center py-8 bg-black text-white font-semibold'>
+    <footer className='flex absolute bottom-0 items-center text-3xl w-full justify-center py-8 bg-black text-white font-semibold'>
       <p className='flex'>Made by Lucas Eitel Cereceda</p>
     </footer>
   );
